e9a2bffb166391b7f2b133a82d51c63dc8d0b595
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-01 19:58:05 +0545

    implementation of routes with their specific components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Gallery from './pages/Gallery'
-import Wisdom from './pages/Wisdom'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import Wisdom from "./pages/Wisdom";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/gallery' element={<Gallery/>}/>
-      <Route path='/wisdom' element={<Wisdom/>}/>
-    </Routes>
-  )
-}
+    <>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/wisdom" element={<Wisdom />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
