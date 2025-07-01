fc7621be736a58b0085bec33617d91833fa87578
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-01 15:24:28 +0545

    set up routing and basic page components (Home, Gallery, Wisdom)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Gallery from './pages/Gallery'
+import Wisdom from './pages/Wisdom'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/gallery' element={<Gallery/>}/>
+      <Route path='/wisdom' element={<Wisdom/>}/>
+    </Routes>
   )
 }
 
---
+++ src/pages/Wisdom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Wisdom = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Wisdom
