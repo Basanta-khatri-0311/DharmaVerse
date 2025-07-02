f5dd5aefc30c8b1be6134798f81f4099711f8a92
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-02 14:55:57 +0545

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="stickt bottom-0 mt-16 py-6 text-center text-sm text-yellow-700 bg-yellow-50 border-t border-yellow-300 font-bold">
+      <p className="max-w-xl mx-auto">
+        © {new Date().getFullYear()}{" "}
+        <span className="font-semibold">DharmaVerse</span> — Spreading Wisdom with Light{" "}
+        <span role="img" aria-label="Om symbol">🕉️</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
