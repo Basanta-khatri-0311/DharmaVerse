03d83ef171c6c385744b41d6d6016ebffc70358c
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-02 18:47:50 +0545

    color update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mt-16 py-6 text-center text-sm text-yellow-700 bg-yellow-50 border-t border-yellow-300 font-bold">
+    <footer className="mt-16 py-6 text-center text-sm text-yellow-700 bg-yellow-100 border-t border-yellow-300 font-bold">
       <p className="max-w-xl mx-auto">
         © {new Date().getFullYear()}{" "}
         <span className="font-semibold">DharmaVerse</span> — Spreading Wisdom with Light{" "}
